80ad78b3af8453d2b35a3b28ccd4864fe4a6ee31
rluisr <detteiu.x14th@gmail.com>
2024-05-12 02:15:10 +0900

    fix: update SubscriptionFailed struct with additional field.

--- subscription.go
+++ subscription.go
@@ -13,8 +13,9 @@ const (
 
 type SubscriptionFailed struct {
 	gorm.Model
-	UserID     string   `gorm:"varchar(255);not null"`
-	CustomerID string   `gorm:"varchar(255);not null"`
-	Email      string   `gorm:"varchar(255);not null"`
-	PlanType   PlanType `gorm:"varchar(255);not null"`
+	UserID         string   `gorm:"varchar(255);not null"`
+	CustomerID     string   `gorm:"varchar(255);not null"`
+	Email          string   `gorm:"varchar(255);not null"`
+	PlanType       PlanType `gorm:"varchar(255);not null"`
+	ReadyForDelete bool     `gorm:"default:false;comment:ユーザーがサイトから退会処理をした場合に true になる。false の場合、CreatedAt から n日後に実行される"`
 }
