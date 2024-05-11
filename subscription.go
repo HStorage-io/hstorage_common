10d6adab6adb7089a4572f3ba0c7d778ba7194aa
rluisr <detteiu.x14th@gmail.com>
2024-05-11 23:07:18 +0900

    feat: add Subscription struct and constants.

---
+++ subscription.go
@@ -0,0 +1,18 @@
+package hstorage_common
+
+import "gorm.io/gorm"
+
+type PlanType string
+
+const (
+	PlanTypeFree     PlanType = "free"
+	PlanTypePremium  PlanType = "premium"
+	PlanTypeBusiness PlanType = "business"
+)
+
+type SubscriptionFailed struct {
+	gorm.Model
+	CustomerID string   `gorm:"varchar(255);not null"`
+	Email      string   `gorm:"varchar(255);not null"`
+	PlanType   PlanType `gorm:"varchar(255);not null"`
+}
