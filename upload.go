ece9fc9e115caedb10dae10e1eab3259d23389ef
rluisr <detteiu.x14th@gmail.com>
2024-06-23 14:28:27 +0900

    feat(perf): implement video thumbnail hashing and signing

--- upload.go
+++ upload.go
@@ -2,7 +2,10 @@ package hstorage_common
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
 	"log"
 	"net"
@@ -284,6 +287,20 @@ func GetThumbnailURL(fileBaseURL string, upload *Upload, password string, isLoca
 	}
 
 	return fmt.Sprintf("%s&url=%s&is_thumb=true&password=%s", baseURL, GetFileURL(fileBaseURL, upload), password)
+}
+
+func GetVideoThumbnailURL(secret, path, fileBaseURL string, upload *Upload) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write([]byte(path))
+	hash := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	hash = strings.TrimRight(hash, "=")
+	hash = strings.ReplaceAll(hash, "+", "-")
+	hash = strings.ReplaceAll(hash, "/", "_")
+	if len(hash) > 40 {
+		hash = hash[:40]
+	}
+
+	return fmt.Sprintf("%s/%s", hash, path)
 }
 
 func ErrFileNameNotProvided() error {
